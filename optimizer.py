218b251673ffe308654a7a8e9b0b27fa8dda10b5
tanner-andrulis <81568882+tanner-andrulis@users.noreply.github.com>
2023-03-06 08:34:48 -0500

    Update to 0.4

--- optimizer.py
+++ optimizer.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict
 from headers import *
 from model import get_energy, get_area
@@ -10,31 +11,34 @@ class ADCRequest:
             tech: float,  # Tech node (nm)
             throughput: float = 0,  # ops/channel/second
             n_adc: int = 1,  # Number of ADCs. Fractions allowed
+            logger: logging.Logger = None,
     ):
         self.bits = bits
         self.tech = tech
         self.throughput = throughput
         self.n_adc = n_adc
+        self.logger = logger
+        assert self.bits > 5, 'Resolution must be >= 6 bits'
 
     def energy_per_op(self, model: Dict) -> float:
         """ Returns energy per operation in Joules. """
         design_params = {ENOB: self.bits, TECH: math.log(self.tech),
                          FREQ: math.log(self.throughput / self.n_adc)}
-        e_per_op = get_energy(design_params, model, True, False)
+        e_per_op = get_energy(design_params, model, True, False, self.logger)
 
-        print('\tAlternative designs:')
+        self.logger.info('\tAlternative designs:')
         for n_adc in range(max(self.n_adc - 5, 1), self.n_adc + 5):
             f = self.throughput / n_adc
             design_params[FREQ] = math.log(f)
             try:
-                e = get_energy(design_params, model, False, False)
+                e = get_energy(design_params, model, False, False, self.logger)
                 a = self.area(model, n_adc)
                 l = '\tCHOSEN > ' if n_adc == self.n_adc else '\t         '
-                print(f'{l}{n_adc:2f} ADCs running at {f:2E}Hz: '
-                      f'{e*1e12:2E}pJ/op, {a/1e6:2E}mm^2')
+                self.logger.info(f'{l}{n_adc:2f} ADCs running at {f:2E}Hz: '
+                                 f'{e*1e12:2E}pJ/op, {a/1e6:2E}mm^2')
             except AssertionError:
                 pass
-        print('')
+        self.logger.info('')
         return e_per_op
 
     def area(self, model: Dict, n_adc_override=-1) -> float:
@@ -42,5 +46,5 @@ class ADCRequest:
         n_adc = self.n_adc if n_adc_override == -1 else n_adc_override
         design_params = {ENOB: self.bits, TECH: math.log(self.tech),
                          FREQ: math.log(self.throughput / n_adc)}
-        design_params[ENRG] = get_energy(design_params, model, False, False) * 1e12
+        design_params[ENRG] = get_energy(design_params, model, False, False, self.logger) * 1e12
         return get_area(design_params, model) * n_adc
